509a0984187c703cd5ff30ce41473ff631902a6b
Stefan Roberto-Ionut <stefanroberto21@stud.ase.ro>
2024-03-17 15:39:11 +0200

    Created first pydantic schema

--- schemas.py
+++ schemas.py
@@ -1,4 +1,23 @@
+import datetime as dt
+from typing import Optional
+
 from pydantic import BaseModel
 
 class ArtistBase(BaseModel):
-    
+    name: str
+    country: str
+    birth_date: dt.datetime
+    bio: Optional[str]
+    genre: str
+
+class ArtistCreate(ArtistBase):
+    pass
+
+class Artist(ArtistBase):
+    id: int
+    listeners: int = 0
+
+    class Config:
+        orm_mode = True
+
+# TODO: Add pydantic schemas for each SQLAlchemy model
