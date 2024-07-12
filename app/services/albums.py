6b949a6e9340096f84cb9be3294e96115b91959f
Stefan Roberto-Ionut <stefanroberto21@stud.ase.ro>
2024-07-12 19:57:57 +0300

    Added new routes and tests.

--- app/services/albums.py
+++ app/services/albums.py
@@ -14,3 +14,41 @@ def create_album(album: schemas.AlbumCreate, session: Session):
     album_dict = jsonable_encoder(db_album)
 
     return schemas.Album(**album_dict)
+
+
+def find_album(album_id: int, session: Session):
+    db_album = session.query(models.Album).filter(models.Album.id == album_id).first()
+
+    return db_album
+
+
+def read_album(album_id: int, session: Session):
+    db_album = find_album(album_id, session)
+    album_dict = jsonable_encoder(db_album)
+
+    return schemas.Album(**album_dict)
+
+
+def update_album(album_id: int, album: schemas.AlbumUpdate, session: Session):
+    db_album = find_album(album_id, session)
+
+    for key, value in album.dict().items():
+        setattr(db_album, key, value)
+
+    session.commit()
+    session.refresh(db_album)
+
+    album_dict = jsonable_encoder(db_album)
+
+    return schemas.Album(**album_dict)
+
+
+def delete_album(album_id: int, session: Session):
+    db_album = find_album(album_id, session)
+
+    session.delete(db_album)
+    session.commit()
+
+    album_dict = jsonable_encoder(db_album)
+
+    return schemas.Album(**album_dict)
