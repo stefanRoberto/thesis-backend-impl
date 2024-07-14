bb910348acf5564dc158894317eed0595c9efc16
Stefan Roberto-Ionut <stefanroberto21@stud.ase.ro>
2024-07-14 10:11:55 +0300

    Hid album routes from repo

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 
 from . import database
-from .routers import artists, albums
+from .routers import artists
 
 app = FastAPI()
 
 database.create_database()
 
 app.include_router(artists.router, prefix='/api')
-app.include_router(albums.router, prefix='/api')
